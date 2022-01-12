e86a44b2f5fd539040e6bf334b6b67a2cb10302c
Konsta <kodu@live.fi>
2022-01-12 16:17:02 +0200

    joasdjfioa

--- WeatherApp.py
+++ WeatherApp.py
@@ -9,6 +9,12 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height = S_Height, width = S_Width,bg="#8585ad")
 canvas.pack()
 
+background_image = tk.PhotoImage(file = './weather_bg.jpg')
+background_label = tk.Label(root, image = background_image)
+background_label.place(relwidth=1, relheight=1)
+
+# Top Frame
+
 frame_top = tk.Frame(root, bg = "#ccf2ff")
 frame_top.place(relx = 0.1, rely = 0.1, relwidth = 0.8 ,relheight = 0.1)
 
@@ -17,6 +23,8 @@ button.place(relx = 0.69, rely = 0.1, relwidth = 0.3 ,relheight = 0.8)
 
 entry= tk.Entry(frame_top, bg= "#f2f2f2",fg="black")
 entry.place(relx=0.01,rely=0.1,relwidth=0.65,relheight=0.8)
+
+# Bottom Frame
 
 frame_bot = tk.Frame(root, bg = "#ccf2ff")
 frame_bot.place(relx = 0.1, rely = 0.3, relwidth = 0.8 ,relheight = 0.6)
